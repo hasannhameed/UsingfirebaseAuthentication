2abe1f3c0b218f00b3cd6433c97cee6b9d7f885d
hasanhameed <hasanhameed858@gmail.com>
2024-07-12 00:59:19 +0530

    local storage used

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './Store.js/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/profile" element={<UserProfile />} />
+        { !authCtx.isLoggedIn &&  <Route path="/auth" element={<AuthPage />} />}
+        { authCtx.isLoggedIn && <Route path="/profile" element={<UserProfile />} />}
+        <Route path='*' element={<HomePage />}/>
         </Routes>
+        
       </Layout>
     </BrowserRouter>
   );
